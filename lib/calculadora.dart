db7711f93c7e10e63826f89779492735c795106d
juliane-maria <julianesousa758@gmail.com>
2022-04-06 22:36:30 -0300

    feat:base calculadora

---
+++ lib/calculadora.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _incrementCounter() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200.0,
+            //Card
+          ),
+          //SizedBox
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [const Text('Display')],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text('7'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('8'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('9'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('x'),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text('4'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('5'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('6'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('-'),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text('1'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('2'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('3'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('+'),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text('+/-'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('0'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('.'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('='),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
