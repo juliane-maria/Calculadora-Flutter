a6e6bd994f05e321fcad418049043bfb3c55cba4
juliane-maria <julianesousa758@gmail.com>
2022-04-13 16:50:11 -0300

    feat:estilo purple

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -23,27 +24,123 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.black,
       body: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 200.0,
             //Card
           ),
           //SizedBox
           Container(
+            child: widgetTextField(),
+          ),
+          Container(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [const Text('Display')],
+              children: const [
+                const SizedBox(
+                  height: 40.0,
+                  //Card
+                ),
+              ],
             ),
           ),
           Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.purple,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextButton(
                   onPressed: () {},
-                  child: Text(
+                  child: const Text(
+                    'C',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '()',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '%',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '/',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.purple,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
                     '7',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '8',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '9',
                     textDirection: TextDirection.ltr,
                     style: TextStyle(
                       fontSize: 30.0,
@@ -53,87 +150,195 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('8'),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('9'),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('x'),
+                  child: const Text(
+                    'x',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.purple,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextButton(
                   onPressed: () {},
-                  child: Text('4'),
+                  child: const Text(
+                    '4',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('5'),
+                  child: const Text(
+                    '5',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('6'),
+                  child: const Text(
+                    '6',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('-'),
+                  child: const Text(
+                    '-',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.purple,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextButton(
                   onPressed: () {},
-                  child: Text('1'),
+                  child: const Text(
+                    '1',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('2'),
+                  child: const Text(
+                    '2',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('3'),
+                  child: const Text(
+                    '3',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('+'),
+                  child: const Text(
+                    '+',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.purple,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextButton(
                   onPressed: () {},
-                  child: Text('+/-'),
+                  child: const Text(
+                    '+/-',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('0'),
+                  child: const Text(
+                    '0',
+                    textDirection: TextDirection.ltr,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('.'),
+                  child: const Text(
+                    '.',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text('='),
+                  child: const Text(
+                    '=',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                  //Card
                 ),
               ],
             ),
@@ -148,4 +353,35 @@ class _MyHomePageState extends State<MyHomePage> {
       // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+widgetTextField() {
+  return Expanded(
+    flex: 1,
+    child: Container(
+      color: Colors.purple,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: AutoSizeText(
+              'TextField',
+              minFontSize: 20.0,
+              maxFontSize: 80.0,
+              maxLines: 1,
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontWeight: FontWeight.w200,
+                decoration: TextDecoration.none,
+                fontSize: 80.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
